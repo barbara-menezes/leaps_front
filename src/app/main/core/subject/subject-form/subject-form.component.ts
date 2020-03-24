6a60078ba9a34151c8b3f6bcd3ccff2c10bb209f
Matheus Carneiro <mcarneiro@sga.pucminas.br>
2020-03-24 14:33:20 -0300

    validação de campos no form e save/reset

--- src/app/main/core/subject/subject-form/subject-form.component.ts
+++ src/app/main/core/subject/subject-form/subject-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-subject-form',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectFormComponent implements OnInit {
 
-  constructor() { }
+  cadastro : FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb : FormBuilder) { }
+
+  ngOnInit() {
+    this.cadastro = this.fb.group({
+      cod_disciplina: ['', [Validators.required, Validators.minLength(6)]],
+      nome_disciplina: ['', [Validators.required]],
+      periodo: [0, [Validators.required, Validators.min(4), Validators.max(10)]],
+      turno: ['', [Validators.required]]
+    });
+  }
+
+  save(): void {
+    if (this.cadastro.invalid) {
+      return;
+    }
+    alert('success' + JSON.stringify(this.cadastro.value, null, 4))
+  }
+
+  resetForm(): void {
+    this.cadastro.reset();
   }
 
 }
