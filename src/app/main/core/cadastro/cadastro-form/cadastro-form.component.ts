aaad7d538a187a196846cce8aba44b1bce5f8bd0
Bárbara <barbara.menezes@live.com>
2020-06-03 00:10:15 -0300

    mostrar/esconder senha monitor

--- src/app/main/core/cadastro/cadastro-form/cadastro-form.component.ts
+++ src/app/main/core/cadastro/cadastro-form/cadastro-form.component.ts
@@ -31,6 +31,19 @@ export class CadastroFormComponent implements OnInit {
     })
 
   ngOnInit() {
+
+    document.getElementById('olho').addEventListener('mousedown', function() {
+      document.getElementById('pass').type = 'text';
+    });
+    
+    document.getElementById('olho').addEventListener('mouseup', function() {
+      document.getElementById('pass').type = 'password';
+    });
+    
+    document.getElementById('olho').addEventListener('mousemove', function() {
+      document.getElementById('pass').type = 'password';
+    });
+
     this.codigo = this.route.snapshot.paramMap.get("codigo");
     this.id = null
     if (!!this.codigo) {
