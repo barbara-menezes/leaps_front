b989f2294421d4d0ca1ff7ae173d480ba1505407
João Pedro Teles de Andrades Pereira <joao.andrades@dtidigital.com.br>
2020-06-05 18:04:58 -0300

    Campo de pesquisa nas telas

--- src/app/main/core/cadastro/cadastro.component.ts
+++ src/app/main/core/cadastro/cadastro.component.ts
@@ -25,6 +25,27 @@ export class CadastroComponent implements OnInit {
     })
   }
 
+  myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 1; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
   
   registerCadastro() {
     this.router.navigateByUrl('/cadastro/cadastro-form')
